4c6bdf569f7d3074b64d29d725b221f6ee6151f2
cxsx-9 <99903983+cxsx-9@users.noreply.github.com>
2023-07-21 18:37:15 +0700

    add scene

--- includes/minirt_define.h
+++ includes/minirt_define.h
@@ -6,15 +6,19 @@
 /*   By: csantivi <csantivi@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 23:36:11 by tkraikua          #+#    #+#             */
-/*   Updated: 2023/07/21 17:01:55 by csantivi         ###   ########.fr       */
+/*   Updated: 2023/07/21 18:36:54 by csantivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_DEFINE_H
 # define MINIRT_DEFINE_H
 
-# define WIN_WIDTH 300
-# define WIN_HEIGHT 300
+// resolution 16 : 9
+// 1280 : 720       for HD
+// 320 : 180        for Rapid rendering mode
+
+# define WIN_WIDTH 320
+# define WIN_HEIGHT 180
 # define TITLE "MINIRT"
 
 # define DBL_MAX 1.7976931348623158e+308
